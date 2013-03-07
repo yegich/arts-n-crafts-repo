eadfe472e647547eb2e34933fe524741a38fd7c4
Yegor Tezikov <yegor.tezikov@gmail.com>
2013-03-07 19:01:27 +0200

    remove

--- algo/quicksort/src/yega/artsandcrafts/algorithms/kosarajus/KosarajusAlgorithm.java
+++ algo/quicksort/src/yega/artsandcrafts/algorithms/kosarajus/KosarajusAlgorithm.java
@@ -1,5 +1,35 @@
 package yega.artsandcrafts.algorithms.kosarajus;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import yega.alogorithms.randomizecontraction.Vertex;
+import yega.artsandcrafts.algorithms.common.AdjacencyList;
+
 public class KosarajusAlgorithm {
 
+	private AdjacencyList graph;
+	private Set<Vertex> explored;
+	
+	public KosarajusAlgorithm(AdjacencyList adjacency) {
+		this.graph = adjacency;
+		explored = new HashSet<Vertex>();
+	}
+	
+	void dfs(AdjacencyList adjacency, Vertex lable) {
+		explored.add(lable);
+		
+		List<Vertex> labledVertices = graph.getVertices(lable);
+		
+		for(Vertex vertex : labledVertices) {
+			if(!explored.contains(vertex)) {
+				dfs(adjacency, vertex);
+			}
+		}
+	}
+
+	public Set<Vertex> getExplored() {
+		return explored;
+	}
 }
--- algo/quicksort/test/yega/artsandcrafts/algorithms/kosarajus/KosarajusAlgorithmTest.java
+++ algo/quicksort/test/yega/artsandcrafts/algorithms/kosarajus/KosarajusAlgorithmTest.java
@@ -1,7 +1,32 @@
 package yega.artsandcrafts.algorithms.kosarajus;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static yega.artsandcrafts.algorithms.common.SccAdjacencyListBuilderTestData.*;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class KosarajusAlgorithmTest {
 
-
+	KosarajusAlgorithm algo;
+	
+	@Before
+	public void setUp() {
+		algo = new KosarajusAlgorithm(TEST_LIST);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testDfsLoopAlgorithm() {
+		
+		algo.dfs(TEST_LIST, V1);
+		
+		assertThat(algo.getExplored(), hasItems(allOf(is(V1), is(V2), is(V3))));
+	}
 }
